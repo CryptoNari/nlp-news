96693fdd7327824cf97258c9b4901318bd883b1a
Andreas Vieten <cryptonari@outlook.com>
2021-07-15 19:26:45 +0200

    seperate dev/prod server

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,6 +12,9 @@ module.exports = {
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    devServer: {
+        port: 8080
+      },
     module: {
         rules: [
             {
